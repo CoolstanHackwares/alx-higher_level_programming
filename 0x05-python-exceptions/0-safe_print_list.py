20aa90cf3a81dd59cef8634ad429b65a0a82d4c9
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-01-22 15:08:17 -0800

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if count < x:
-                print("{:d}".formt[i], end=" ")
+                print("{:d}".format(my_list[count]), end="")
             count += 1
         print()
     except TypeError:
