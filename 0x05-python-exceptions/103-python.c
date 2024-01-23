406d296f1583176fbba7940afe53d8c7e7c5e75a
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-01-23 15:44:43 -0800

    CPython #2: PyFloatObject

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
@@ -6,6 +5,61 @@
 #include <floatobject.h>
 #include <stdlib.h>
 #include <float.h>
+
+/**
+ * print_python_float - A function that prints information about
+ * a Python float object
+ * @p: A pointer to the Python float object
+ * This program conforms to the betty documentation style
+ **/
+
+void print_python_float(PyObject *p)
+{
+	PyFloatObject *f = (PyFloatObject *)p;
+	double d = f->ob_fval;
+	char *str = NULL;
+
+	printf("[.] float object info\n");
+	if (!PyFloat_Check(f))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	str = PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", str);
+}
+
+/**
+ * print_python_bytes - A function that prints information about a Python
+ * bytes object
+ * @p: A pointer to the Python bytes object
+ * This program conforms to the betty documentation style
+ **/
+
+void print_python_bytes(PyObject *p)
+{
+	long unsigned int size;
+	unsigned int i;
+	char *trying_str = NULL;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	size = ((PyVarObject *)p)->ob_size;
+	trying_str = ((PyBytesObject *)p)->ob_sval;
+	printf("  size: %lu\n", size);
+	printf("  trying string: %s\n", trying_str);
+	if (size < 10)
+		printf("  first %lu bytes:", size + 1);
+	else
+		printf("  first 10 bytes:");
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02hhx", trying_str[i]);
+	printf("\n");
+}
 
 /**
  * print_python_list - A funnction that prints information about a Python list
@@ -38,56 +92,4 @@ void print_python_list(PyObject *p)
 		if (!strcmp(type, "float"))
 			print_python_float(list->ob_item[i]);
 	}
-}
-
-/**
- * print_python_bytes - A function that prints information about a Python
- * bytes object
- * @p: A pointer to the Python bytes object
- * This program conforms to the betty documentation style
- **/
-
-void print_python_bytes(PyObject *p)
-{
-	long unsigned int size;
-	unsigned int i;
-	char *trying_str = NULL;
-
-	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
-	size = ((PyVarObject *)p)->ob_size;
-	trying_str = ((PyBytesObject *)p)->ob_sval;
-	printf("  size: %lu\n", size);
-	printf("  trying string: %s\n", trying_str);
-	printf("  first %lu bytes:", size < 10 ? size + 1 : 10);
-	for (i = 0; i < 10 && i < size; i++)
-		printf(" %02hhx", trying_str[i]);
-	printf("\n");
-}
-
-/**
- * print_python_float - A function that prints information about
- * a Python float object
- * @p: A pointer to the Python float object
- * This program conforms to the betty documentation style
- **/
-
-void print_python_float(PyObject *p)
-{
-	PyFloatObject *f = (PyFloatObject *)p;
-	double d = f->ob_fval;
-	char *str = NULL;
-
-	printf("[.] float object info\n");
-	if (!PyFloat_Check(f))
-	{
-		printf("  [ERROR] Invalid Float Object\n");
-		return;
-	}
-	str = PyOS_double_to_string(d, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-	printf("  value: %s\n", str);
 }
