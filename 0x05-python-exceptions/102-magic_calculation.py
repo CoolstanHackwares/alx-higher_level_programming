647d904738d129871997fcceac05cc6e75ca75ea
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-01-23 15:33:50 -0800

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,7 +4,7 @@ import dis
 
 def magic_calculation(a, b):
     result = 0
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
