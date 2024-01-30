e63dd293195a5164a1fcf5e13fbcd54972d6a20f
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-01-29 21:09:58 -0800

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,65 +1,36 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col, n):
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, n)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-def solve_nqueens(board, row, n):
-    if row == n:
-        solution = []
-        for i in range(n):
-            for j in range(n):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        print(solution)
-        return
-
-    for col in range(n):
-        if is_safe(board, row, col, n):
-            board[row][col] = 1
-            solve_nqueens(board, row + 1, n)
-            board[row][col] = 0  # Backtrack
-
-def nqueens(n):
-    if not isinstance(n, int):
-        print("N must be a number")
-        sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    solve_nqueens(board, 0, n)
+def print_solution(solution):
+    print([[f"{row}{col}"  for col in solution]])
 
 if __name__ == "__main__":
+
     if len(sys.argv) != 2:
-        print("Usage: ./101-nqueens.py N")
+        print("Usage: nqueens N")
         sys.exit(1)
 
-    try:
-        N = int(sys.argv[1])
-        nqueens(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)")
-        sys.exit(1)
 
-    try:
-        N = int(sys.argv[1])
-        nqueens(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+    solutions = []
+
+    def is_valid(col_positions):
+
+        def search(col_positions=[]):
+            if len(col_positions) == N:
+                solutions.append(col_positions)
+            return
+
+        for col in range(N):
+
+
+            search()
+
+    for solution in solutions:
+        print_solution(solution)    for col in range(N):
+            # Recursive search logic
+
+    search()
+
+    # Print each solution  
+    for solution in solutions:
+        print_solution(solution)
