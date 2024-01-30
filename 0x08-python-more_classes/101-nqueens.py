00f9351c58723b52a0853aff0c7732d86bc05523
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-01-29 21:12:31 -0800

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,36 +1,62 @@
 #!/usr/bin/python3
-import sys
+"""
+nqueens program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
+"""
 
-def print_solution(solution):
-    print([[f"{row}{col}"  for col in solution]])
+
+from sys import argv
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 2:
+    a = []
+    if len(argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
 
+    # initialize the answer list
+    for i in range(n):
+        a.append([i, None])
 
-    solutions = []
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == a[x][1]:
+                return True
+        return False
 
-    def is_valid(col_positions):
+    def reject(x, y):
+        """determines whether or not to reject the solution"""
+        if (already_exists(y)):
+            return False
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
 
-        def search(col_positions=[]):
-            if len(col_positions) == N:
-                solutions.append(col_positions)
-            return
+    def clear_a(x):
+        """clears the answers from the point of failure on"""
+        for i in range(x, n):
+            a[i][1] = None
 
-        for col in range(N):
+    def nqueens(x):
+        """recursive backtracking function to find the solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
+                else:
+                    nqueens(x + 1)  # moves on to next x value to continue
 
-
-            search()
-
-    for solution in solutions:
-        print_solution(solution)    for col in range(N):
-            # Recursive search logic
-
-    search()
-
-    # Print each solution  
-    for solution in solutions:
-        print_solution(solution)
+    # start the recursive process at x = 0
+    nqueens(0)eens(0)
