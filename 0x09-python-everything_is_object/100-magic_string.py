da77d827579da59aff8b2f00fa2d7df418c86fa4
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-01-30 14:17:32 -0800

    #pythonic fix

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(counter=[0]):
     counter[0] += 1
-    return str("BestSchool, " * (counter[0] -1)) + "BestSchool"
+    return str("BestSchool, " * (counter[0] - 1)) + "BestSchool"
