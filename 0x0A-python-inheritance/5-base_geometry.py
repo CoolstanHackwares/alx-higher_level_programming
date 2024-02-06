004411c9dc3c3418e2efe64f35c59c48c0afab52
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-02-05 18:35:30 -0800

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    An empty class representing the base geometry."""
+
+class BaseGeometry:
+    """Determines if the empty class represents the base geometry.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
