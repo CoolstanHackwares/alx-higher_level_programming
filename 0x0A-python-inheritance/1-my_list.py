80815c9407daf9c52e498a4bd2be8000252e6b99
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-02-05 18:15:45 -0800

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Module 1-my_list.
-Creates a class inheriting fm the list class.
+This creates a class inheriting from the list class.
 """
 
+
 class MyList(list):
-    """Class MyList inherits fm list."""
+    """Class MyList inherits from the list."""
 
     def print_sorted(self):
-        """Prints the list, in ascending sort."""
+        """Prints the list, in ascending order."""
 
         new_list = self[:]
         new_list.sort()
-        print("{}".format(new_list)
+        print("{}".format(new_list))
