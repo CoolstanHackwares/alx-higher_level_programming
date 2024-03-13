a62744bcfc067d51e8b2bcc32d6d2a609def1018
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-03-13 15:53:12 -0700

    Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -7,7 +7,7 @@
 
 SELECT name, SUM(tv_shows_ratings.rate) AS 'rating'
 FROM tv_genres
-INNER JOIN tv_shows_genres ON tv_genres.id = tv_shows_genre.genre_id
-INNER JOIN tv_shows_ratings ON tv_shows_genres.show_id = tv_shows_ratings.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genre.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY name
 ORDER BY rating DESC;
