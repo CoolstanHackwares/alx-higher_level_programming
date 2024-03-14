724c41240ed9259ec71cbcf2ce8caf5aa82fbdae
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-03-13 21:56:33 -0700

    Only comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,9 +5,9 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title
+SELECT title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+ORDER BY title ASC;
