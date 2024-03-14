8f933bed71f7a29550f808da2d284a17fc497406
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-03-14 02:08:27 -0700

    Cities of California fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- Select the id of California from the states table
-SELECT id FROM states WHERE name = 'California';
-
 -- Use the id obtained from the previous query to select cities of California
+
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
