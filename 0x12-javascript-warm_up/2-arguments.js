2538964b6bc4f18707e5a1d3d741c97140ddaebd
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-08 16:02:11 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0)
+if (process.argv.length === 2)
 {
- console.log("No argument");
+  console.log('No argument');
 }
-else if (args.length === 1)
+else if (process.argv.length === 3)
 {
- console.log("Argument found");
+  console.log('Argument found');
 }
 else
 {
- console.log("Arguments found");
+  console.log('Arguments found');
 }
