64893a05630453f9d90d3021abf9357022c8bbc1
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-09 16:01:04 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const Square = require('./5-square.js');
 module.exports = class Square extends Square {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
     }
   }
 };
