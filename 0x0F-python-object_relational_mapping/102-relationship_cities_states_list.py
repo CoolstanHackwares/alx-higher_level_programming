11238a2582e4720e5e4ede3ad72f3d0f8226545b
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-14 15:59:50 -0700

    From city

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     import sys
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import session
+    from sqlalchemy.orm import Session
     from relationship_state import Base, State
     from relationship_city import City
     from sqlalchemy.schema import Table
