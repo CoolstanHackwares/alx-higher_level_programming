7472bf38dda8ec7a19218c5db43d28e0ff6d7c69
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-25 15:38:29 -0700

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request with file contents as JSON body and display response body
-curl -s -X POST "$url" -H "Content-Type: application/json" -d @"$file"
+curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
