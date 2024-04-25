a9a27366cd34d11cecc887884a792bb87186cc35
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-25 16:59:51 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,7 @@ def find_peak(list_of_integers):
     high = len(list_of_integers)
 
     while low < high:
-        mid = ((high - low) // 2) + low
+        mid = (low + high) // 2
 
         # Check if mid is a peak
         if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) \
@@ -25,3 +25,4 @@ def find_peak(list_of_integers):
             low = mid + 1
 
     return None
+
