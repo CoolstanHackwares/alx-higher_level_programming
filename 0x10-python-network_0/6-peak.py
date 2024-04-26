7c250e72b072cb29a1af7afba44b1a2bfa9b252c
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-26 03:02:22 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Finds a peak in a list of unsorted integers."""
 
 def find_peak(list_of_integers):
@@ -13,7 +14,8 @@ def find_peak(list_of_integers):
         mid = (low + high) // 2
 
         if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) \
-            and (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+                and (mid == len(list_of_integers) - 1 or \
+                     list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
 
         if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
@@ -22,4 +24,3 @@ def find_peak(list_of_integers):
             low = mid + 1
 
     return None
-
