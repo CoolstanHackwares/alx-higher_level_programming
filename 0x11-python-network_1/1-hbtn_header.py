e36a43ff99f4699bfe8e1a5469610b591e6a3e15
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-26 18:11:08 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,5 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.headers.get('X-Request-Id')
