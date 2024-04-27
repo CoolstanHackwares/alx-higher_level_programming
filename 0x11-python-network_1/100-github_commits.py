43bb3bb7fe3d56297bceccdf9ca705b8cf48f902
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-26 18:30:53 -0700

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that prints the 10 most recent commits from a GitHub repository"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+    response = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
