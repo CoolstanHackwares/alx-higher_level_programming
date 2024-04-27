e00e1523e64d8aa9f1daa547cad5a1d7a3652330
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-04-26 17:36:01 -0700

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""A script that sends a POST request to a URL 
-with an email address as a parameter"""
+"""A script that sends a POST request to a URL with an email address as a
+parameter and displays the body of the response"""
+
 import sys
 import requests
 
@@ -9,5 +10,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     response = requests.post(url, data={'email': email})
-    print("Your email is: {}".format(email))
     print(response.text)
