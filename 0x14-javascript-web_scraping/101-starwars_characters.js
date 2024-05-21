55454d6b9062f42eceaaf51e9aa241a494fe117a
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-05-21 05:44:36 -0700

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,43 +1,35 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-if (process.argv.length !== 3) {
-  console.log('Usage: ./101-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(filmUrl, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
     console.error('Error fetching movie details:', error);
     return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error(`Error: Received status code ${response.statusCode}`);
+  const characters = JSON.parse(body).characters;
+  printCharacters(characters, 0);
+});
+
+function printCharacters (characters, index) {
+  if (index >= characters.length) {
     return;
   }
 
-  const film = JSON.parse(body);
-  const characterUrls = film.characters;
+  request(characters[index], function (error, response, body) {
+    if (error) {
+      console.error('Error fetching character details:', error);
+      return;
+    }
 
-  characterUrls.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error fetching character details:', error);
-        return;
-      }
+    if (response.statusCode !== 200) {
+      console.error(`Error: Received status code ${response.statusCode}`);
+      return;
+    }
 
-      if (response.statusCode !== 200) {
-        console.error(`Error: Received status code ${response.statusCode}`);
-        return;
-      }
-
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
+    console.log(JSON.parse(body).name);
+    printCharacters(characters, index + 1);
   });
-});
+}
