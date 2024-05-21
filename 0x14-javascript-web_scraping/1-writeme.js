aecfa2d6e4a145b4282775e4cf9ded628673fc18
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-05-21 07:28:05 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-if (!filePath || !content) {
-  console.error('Please provide both the file path and content as arguments.');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, (err) => {
-  if (err) {
-    console.error(`Error writing file: ${err.message}`);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
